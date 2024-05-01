c2dce943f65b6c6a120156bfc8891425d21c6fc1
pmwhartenby@gmail.com <pmwhartenby@gmail.com>
2024-05-01 15:39:05 -0400

    client server type def

--- PIR/DB/Client.go
+++ PIR/DB/Client.go
@@ -1,6 +1,23 @@
 package db
 
+import matrix "pir/PIR/Matrix"
+
 type Client struct {
+	server *Server
+}
+
+var HintS matrix.Matrix
+var HintC matrix.Matrix
+var Hint matrix.Matrix
+var singleHint = SingleHintReply{Hint: Hint}
+
+var hasHint = false
+
+func GetSingleHint(server *Server) {
+	if hasHint {
+		server.Hint(singleHint)
+	}
+
 }
 
 func (C *Client) MakeSingleQuery(r int, c int) {
